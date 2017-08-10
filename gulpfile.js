9885443cf8bf66ade69ff4fa3ddd28d39ecfa9f8
wenzhongzheng <andrew198712@gmail.com>
2017-08-09 22:43:42 -0400

    project starter

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ gulp.task('serve', ['sass'], function(){
     });
 
     gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'],['sass']);
-    gulp.watch("src/*.html").on('change', browserSync.reload());
+    gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
 // Move Font Awesome Fonts folder to src
